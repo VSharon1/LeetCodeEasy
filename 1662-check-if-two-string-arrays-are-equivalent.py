82464a8aff5a991e900a831e855211d13e949ea2
VSharon1 <vs1.github@pm.me>
2025-03-30 10:34:14 +0200

    Changed variable names

--- 1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent.py
@@ -3,27 +3,27 @@ from typing import List
 
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        word1_i = 0
-        word2_i = 0
-        word1_char_i = 0
-        word2_char_i = 0
+        w1_i = 0
+        w2_i = 0
+        w1_char_i = 0
+        w2_char_i = 0
 
-        while word1_i < len(word1) and word2_i < len(word2):
-            if word1[word1_i][word1_char_i] != word2[word2_i][word2_char_i]:
+        while w1_i < len(word1) and w2_i < len(word2):
+            if word1[w1_i][w1_char_i] != word2[w2_i][w2_char_i]:
                 return False
 
-            word1_char_i += 1
-            word2_char_i += 1
+            w1_char_i += 1
+            w2_char_i += 1
 
-            if word1_char_i == len(word1[word1_i]):
-                word1_i += 1
-                word1_char_i = 0
+            if w1_char_i == len(word1[w1_i]):
+                w1_i += 1
+                w1_char_i = 0
 
-            if word2_char_i == len(word2[word2_i]):
-                word2_i += 1
-                word2_char_i = 0
+            if w2_char_i == len(word2[w2_i]):
+                w2_i += 1
+                w2_char_i = 0
 
-        if word1_i != len(word1) or word2_i != len(word2):
+        if w1_i != len(word1) or w2_i != len(word2):
             return False
 
         return True
