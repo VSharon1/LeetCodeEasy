402a0c1b3c3db5366f528c35593b50046c17619b
VSharon1 <vsharon1@pm.me>
2025-06-09 19:28:18 +0200

    35 - done

---
+++ 35-search-insert-position.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            guess = nums[mid]
+
+            if guess == target:
+                return mid
+            elif guess > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
