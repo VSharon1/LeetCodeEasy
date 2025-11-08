fb5a3ceef0e09bf67c41908178de8b237475d575
VSharon1 <vs1.github@pm.me>
2025-11-08 10:41:55 +0100

    141 - done

---
+++ 141-linked-list-cycle.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self) -> str:
+        return f"ListNode(val={self.val}, next ={self.next})"
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
